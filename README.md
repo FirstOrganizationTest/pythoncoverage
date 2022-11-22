2cc9f04e38aaac7fdc7368572eb5ac487a5eb8f1
rru <ankahikahani@gmail.com>
2022-11-22 12:13:37 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+To Generate coverage reports we are executing the following command
+```
+coverage run -m unittest <file_names>
+coverage xml
+```
---
+++ jello.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    print(f"Adding {a}, {b}")
+    return a + b
+
+def sub(a, b):
+    print(f"Subtracting {a}, {b}")
+    return a - b
+
+if __name__=="__main__":
+    print("Jello")
